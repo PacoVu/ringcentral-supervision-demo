e31407354e8e5e17eaf934d76645ba676182f6c3
PacoVu <paco_vu@yahoo.com>
2020-01-22 11:07:06 -0800

    update 003

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.get('/recording', cors(), async (req, res) => {
 })
 
 // Anything that doesn't match the above, send back the index.html file
-app.get('*', (req, res) => {
+app.get('*', async (req, res) => {
   //res.sendFile(path.join(__dirname + '/client/build/index.html'))
   //console.log("START NOTIFICATION")
   //startNotification()
