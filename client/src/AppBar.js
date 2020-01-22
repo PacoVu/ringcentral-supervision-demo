3a1531a86d99ab7e50b9e9ae2d73c02f18b14e0c
PacoVu <paco_vu@yahoo.com>
2020-01-22 15:05:21 -0800

    update 014

---
+++ client/src/AppBar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import axios from "axios";
+
+export default class AppBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn : false
+    };
+
+  }
+
+  async componentDidMount() {
+    this.eventSource.addEventListener('loginEvent', (e) => this.updateLogin(JSON.parse(e.data)));
+
+  }
+
+  updateLogin(data) {
+    this.setState(prevState => ({
+          isLoggedIn: true
+        }))
+  }
+
+  async login(agent) {
+    const response = await axios.get("/login")
+    //console.log(response.data)
+  }
+
+
+  render() {
+    return (
+      <span className="App">Test</span>
+    );
+  }
+}
