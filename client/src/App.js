1b0bb2fce8fe19fb2114598ca78177cc93b00574
PacoVu <paco_vu@yahoo.com>
2020-03-22 18:14:49 -0700

    Update App.js
    - Enabled test on heroku

--- client/src/App.js
+++ client/src/App.js
@@ -73,8 +73,10 @@ export default class App extends Component {
     };
     this.negativestepsColors = ['#3da940','#3da940','#3da940','#53b83a','#84c42b','#f1bc00','#ed8d00','#d12000']
     this.isLoggedIn = false;
-    this.eventSource = new EventSource('http://localhost:5000/events');
-    //this.eventSource = new EventSource('/events');
+    // For testing in localhost
+    //this.eventSource = new EventSource('http://localhost:5000/events');
+    // For testing in remote server
+    this.eventSource = new EventSource('/events');
   }
 
   async componentDidMount() {
